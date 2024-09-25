537226b61f2b5f26bba803a5e99884406fbebbc6
graymicro <alirezaallahyarian@gmail.com>
2024-09-25 14:52:53 +0330

    dynamic single-projects

--- single-projects.php
+++ single-projects.php
@@ -11,22 +11,23 @@
         <div class="singleProjectNameInfoAwardsWrapper">
             <div class="singleProjectNameInfoWrapper">
                 <div class="projectNameDescriptionWrapper">
-                    <div class="projectLogo"><img src="./assets/img/sample/project-logo-1.png" alt=""></div>
-                    <h1>Project Name</h1>
+                    <div class="projectLogo"><img src="<?php ThemeAssets('img/sample/project-logo-1.png'); ?>" alt="">
+                    </div>
+                    <h1><?php get_the_title(); ?></h1>
                     <div class="description">
-                        <p>Communicate that Designex offers a full spectrum of services, from luxury renovation.</p>
+                        <p><?php get_the_excerpt(); ?></p>
                     </div>
                 </div>
-                <div class="projectsFeatureWrapper">
-                    <div class="featureWrap"><span>Client :</span><span>Ms. Some one</span></div>
-                    <div class="featureWrap"><span>Architect :</span><span>team member names goes here</span></div>
-                    <div class="featureWrap"><span>Mechanical :</span><span>Team member</span></div>
-                    <div class="featureWrap"><span>Location :</span><span>the location name</span></div>
-                    <div class="featureWrap"><span>Client :</span><span>Ms. Some one</span></div>
-                    <div class="featureWrap"><span>Architect :</span><span>team member names goes here</span></div>
-                    <div class="featureWrap"><span>Mechanical :</span><span>Team member</span></div>
-                    <div class="featureWrap"><span>Location :</span><span>the location name</span></div>
-                </div>
+                <?php if (have_rows('features')): ?>
+                    <div class="projectsFeatureWrapper">
+                        <?php while (have_rows('features')):
+                            the_row(); ?>
+                            <div class="featureWrap"><span><?php echo get_sub_field('title'); ?>
+                                    :</span><span><?php echo get_sub_field('name'); ?></span></div>
+                        <?php endwhile; ?>
+
+                    </div>
+                <?php endif; ?>
             </div>
             <div class="singleProjectAwardsWrapper">
                 <a href="#" target="_blank" class="awardsTitleLogoWrapper">
@@ -46,166 +47,84 @@
             </div>
         </div>
         <div class="singleProjectVideoMapContainer">
-            <div class="singleProjectVideoContainer">
-                <video autoplay muted loop playsinline preload="auto" poster="" id="projectVideo"
-                    data-url="./assets/video/coverr-city-skyscrapers-471-1080p.mp4">
-                    <source src="./assets/video/coverr-city-skyscrapers-471-1080p.mp4" type="video/mp4">
-                </video>
-                <div id="playVideo">
-                    <img src="./assets/img/outer-circle.svg" alt="circle text">
-                    <img src="./assets/img/inner-icon-play.svg" alt="play icon">
+            <?php $video = get_field('video');
+            $poster = get_field('poster');
+            if ($video): ?>
+                <div class="singleProjectVideoContainer">
+                    <video autoplay muted loop playsinline preload="auto" poster="<?php echo $poster; ?>" id="projectVideo"
+                        data-url="<?php echo $video; ?>">
+                        <source src="<?php echo $video; ?>" type="video/mp4">
+                    </video>
+                    <div id="playVideo">
+                        <img src="<?php ThemeAssets('img/outer-circle.svg'); ?>" alt="circle text">
+                        <img src="<?php ThemeAssets('img/inner-icon-play.svg'); ?>" alt="play icon">
+                    </div>
                 </div>
-            </div>
-            <div id="singleProjectMap">
-                <img src="./assets/img/sample/map.jpg" alt="">
-            </div>
+            <?php endif; ?>
+            <?php
+            $location = get_field('location');
+            if ($location): ?>
+                <div id="singleProjectMap">
+                    <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>"
+                        data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+                    <!-- <img src="./assets/img/sample/map.jpg" alt=""> -->
+                </div>
+            <?php endif; ?>
         </div>
     </section>
     <section class="singleProjectContent"> <!-- It is sample, Gutenberg goes here ok Alireza!? -->
         <div class="columnTwo">
-            <div class="box">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Eros in cursus turpis massa tincidunt. Pellentesque sit amet porttitor eget
-                    dolor morbi non arcu risus. Facilisis magna etiam tempor orci eu lobortis. Gravida cum sociis
-                    natoque penatibus et magnis. Lorem mollis aliquam ut porttitor leo a. Vitae turpis massa sed
-                    elementum tempus egestas sed sed risus. In vitae turpis massa sed elementum tempus egestas. Facilisi
-                    etiam dignissim diam quis enim lobortis scelerisque. Blandit libero volutpat sed cras ornare arcu
-                    dui vivamus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id.</p>
-                <p>Imperdiet dui accumsan sit amet nulla facilisi. Tincidunt augue interdum velit euismod in
-                    pellentesque. Semper eget duis at tellus at urna condimentum mattis pellentesque. Pellentesque elit
-                    ullamcorper dignissim cras tincidunt lobortis feugiat. At urna condimentum mattis pellentesque id
-                    nibh tortor id. Lectus urna duis convallis convallis tellus id interdum velit. Mollis nunc sed id
-                    semper risus in hendrerit gravida rutrum. Suspendisse in est ante in nibh. Dignissim enim sit amet
-                    venenatis. Faucibus in ornare quam viverra orci. Egestas tellus rutrum tellus pellentesque eu
-                    tincidunt tortor aliquam nulla.</p>
-                <p>Magna sit amet purus gravida. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.
-                    Sem integer vitae justo eget magna fermentum iaculis eu. Enim blandit volutpat maecenas volutpat
-                    blandit aliquam etiam erat velit. Adipiscing elit pellentesque habitant morbi tristique senectus et
-                    netus et. Ut pharetra sit amet aliquam id diam maecenas. Sed risus pretium quam vulputate. Eu non
-                    diam phasellus vestibulum lorem sed risus. Ipsum a arcu cursus vitae congue. Felis eget nunc
-                    lobortis mattis aliquam. Cras semper auctor neque vitae tempus quam. In dictum non consectetur a
-                    erat nam at. Massa tincidunt dui ut ornare lectus sit amet est. Viverra mauris in aliquam sem
-                    fringilla ut morbi. Risus ultricies tristique nulla aliquet enim.</p>
-                <div class="white-space" style="height: 25px"></div>
-                <img src="./assets/img/sample/project-1.png" alt="">
-                <div class="white-space" style="height: 25px"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Eros in cursus turpis massa tincidunt. Pellentesque sit amet porttitor eget
-                    dolor morbi non arcu risus. Facilisis magna etiam tempor orci eu lobortis. Gravida cum sociis
-                    natoque penatibus et magnis. Lorem mollis aliquam ut porttitor leo a. Vitae turpis massa sed
-                    elementum tempus egestas sed sed risus. In vitae turpis massa sed elementum tempus egestas. Facilisi
-                    etiam dignissim diam quis enim lobortis scelerisque. Blandit libero volutpat sed cras ornare arcu
-                    dui vivamus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id.</p>
-                <p>Imperdiet dui accumsan sit amet nulla facilisi. Tincidunt augue interdum velit euismod in
-                    pellentesque. Semper eget duis at tellus at urna condimentum mattis pellentesque. Pellentesque elit
-                    ullamcorper dignissim cras tincidunt lobortis feugiat. At urna condimentum mattis pellentesque id
-                    nibh tortor id. Lectus urna duis convallis convallis tellus id interdum velit. Mollis nunc sed id
-                    semper risus in hendrerit gravida rutrum. Suspendisse in est ante in nibh. Dignissim enim sit amet
-                    venenatis. Faucibus in ornare quam viverra orci. Egestas tellus rutrum tellus pellentesque eu
-                    tincidunt tortor aliquam nulla.</p>
-                <p>Magna sit amet purus gravida. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.
-                    Sem integer vitae justo eget magna fermentum iaculis eu. Enim blandit volutpat maecenas volutpat
-                    blandit aliquam etiam erat velit. Adipiscing elit pellentesque habitant morbi tristique senectus et
-                    netus et. Ut pharetra sit amet aliquam id diam maecenas. Sed risus pretium quam vulputate. Eu non
-                    diam phasellus vestibulum lorem sed risus. Ipsum a arcu cursus vitae congue. Felis eget nunc
-                    lobortis mattis aliquam. Cras semper auctor neque vitae tempus quam. In dictum non consectetur a
-                    erat nam at. Massa tincidunt dui ut ornare lectus sit amet est. Viverra mauris in aliquam sem
-                    fringilla ut morbi. Risus ultricies tristique nulla aliquet enim.</p>
-                <div class="white-space" style="height: 25px"></div>
-                <img src="./assets/img/sample/single-project-1.png" alt="">
-            </div>
-            <div class="box">
-                <img src="./assets/img/sample/project-2.png" alt="">
-                <div class="white-space" style="height: 25px"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Eros in cursus turpis massa tincidunt. Pellentesque sit amet porttitor eget
-                    dolor morbi non arcu risus. Facilisis magna etiam tempor orci eu lobortis. Gravida cum sociis
-                    natoque penatibus et magnis. Lorem mollis aliquam ut porttitor leo a. Vitae turpis massa sed
-                    elementum tempus egestas sed sed risus. In vitae turpis massa sed elementum tempus egestas. Facilisi
-                    etiam dignissim diam quis enim lobortis scelerisque. Blandit libero volutpat sed cras ornare arcu
-                    dui vivamus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id.</p>
-                <p>Imperdiet dui accumsan sit amet nulla facilisi. Tincidunt augue interdum velit euismod in
-                    pellentesque. Semper eget duis at tellus at urna condimentum mattis pellentesque. Pellentesque elit
-                    ullamcorper dignissim cras tincidunt lobortis feugiat. At urna condimentum mattis pellentesque id
-                    nibh tortor id. Lectus urna duis convallis convallis tellus id interdum velit. Mollis nunc sed id
-                    semper risus in hendrerit gravida rutrum. Suspendisse in est ante in nibh. Dignissim enim sit amet
-                    venenatis. Faucibus in ornare quam viverra orci. Egestas tellus rutrum tellus pellentesque eu
-                    tincidunt tortor aliquam nulla.</p>
-                <p>Magna sit amet purus gravida. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.
-                    Sem integer vitae justo eget magna fermentum iaculis eu. Enim blandit volutpat maecenas volutpat
-                    blandit aliquam etiam erat velit. Adipiscing elit pellentesque habitant morbi tristique senectus et
-                    netus et. Ut pharetra sit amet aliquam id diam maecenas. Sed risus pretium quam vulputate. Eu non
-                    diam phasellus vestibulum lorem sed risus. Ipsum a arcu cursus vitae congue. Felis eget nunc
-                    lobortis mattis aliquam. Cras semper auctor neque vitae tempus quam. In dictum non consectetur a
-                    erat nam at. Massa tincidunt dui ut ornare lectus sit amet est. Viverra mauris in aliquam sem
-                    fringilla ut morbi. Risus ultricies tristique nulla aliquet enim.</p>
-                <div class="white-space" style="height: 25px"></div>
-                <img src="./assets/img/sample/project-03.webp" alt="">
-                <div class="white-space" style="height: 25px"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Eros in cursus turpis massa tincidunt. Pellentesque sit amet porttitor eget
-                    dolor morbi non arcu risus. Facilisis magna etiam tempor orci eu lobortis. Gravida cum sociis
-                    natoque penatibus et magnis. Lorem mollis aliquam ut porttitor leo a. Vitae turpis massa sed
-                    elementum tempus egestas sed sed risus. In vitae turpis massa sed elementum tempus egestas. Facilisi
-                    etiam dignissim diam quis enim lobortis scelerisque. Blandit libero volutpat sed cras ornare arcu
-                    dui vivamus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id.</p>
-                <p>Imperdiet dui accumsan sit amet nulla facilisi. Tincidunt augue interdum velit euismod in
-                    pellentesque. Semper eget duis at tellus at urna condimentum mattis pellentesque. Pellentesque elit
-                    ullamcorper dignissim cras tincidunt lobortis feugiat. At urna condimentum mattis pellentesque id
-                    nibh tortor id. Lectus urna duis convallis convallis tellus id interdum velit. Mollis nunc sed id
-                    semper risus in hendrerit gravida rutrum. Suspendisse in est ante in nibh. Dignissim enim sit amet
-                    venenatis. Faucibus in ornare quam viverra orci. Egestas tellus rutrum tellus pellentesque eu
-                    tincidunt tortor aliquam nulla.</p>
-                <p>Magna sit amet purus gravida. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci.
-                    Sem integer vitae justo eget magna fermentum iaculis eu. Enim blandit volutpat maecenas volutpat
-                    blandit aliquam etiam erat velit. Adipiscing elit pellentesque habitant morbi tristique senectus et
-                    netus et. Ut pharetra sit amet aliquam id diam maecenas. Sed risus pretium quam vulputate. Eu non
-                    diam phasellus vestibulum lorem sed risus. Ipsum a arcu cursus vitae congue. Felis eget nunc
-                    lobortis mattis aliquam. Cras semper auctor neque vitae tempus quam. In dictum non consectetur a
-                    erat nam at. Massa tincidunt dui ut ornare lectus sit amet est. Viverra mauris in aliquam sem
-                    fringilla ut morbi. Risus ultricies tristique nulla aliquet enim.</p>
-            </div>
+            <?php the_content(); ?>
         </div>
     </section>
-    <section class="relatedProjectsContainer">
-        <div class="titleWrap">
-            <h2 class="title-roboto">related projects</h2>
-        </div>
-        <div class="relatedProjectsWrapper">
-            <div class="relatedProjectsWrap">
-                <div class="projectMedia">
-                    <img src="./assets/img/sample/project-01.webp" alt="">
+    <?php
+    $categories = get_the_category();
+    if ($categories) {
+        $category_ids = array();
+        foreach ($categories as $category) {
+            $category_ids[] = $category->term_id;
+        }
+
+        $related_args = array(
+            'category__in' => $category_ids,
+            'post__not_in' => array(get_the_ID()), 
+            'posts_per_page' => 3 
+        );
+
+        $related_query = new WP_Query($related_args);
+
+    
+        if ($related_query->have_posts()) { ?>
+            <section class="relatedProjectsContainer">
+                <div class="titleWrap">
+                    <h2 class="title-roboto">related projects</h2>
                 </div>
-                <a href="" class="project-info">
-                    <p class="project-name">Project Name</p>
-                    <div class="year-location">
-                        <span>2022 - Dubai</span>
-                    </div>
-                </a>
-            </div>
-            <div class="relatedProjectsWrap">
-                <div class="projectMedia">
-                    <img src="./assets/img/sample/project-02.webp" alt="">
+                <div class="relatedProjectsWrapper">
+                    <?php while ($related_query->have_posts()) {
+                        $related_query->the_post(); ?>
+
+                        <div class="relatedProjectsWrap">
+                            <div class="projectMedia">
+                                <?php if (has_post_thumbnail()): ?>
+                                    <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>">
+                                <?php endif;?>   
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="project-info">
+                                <p class="project-name"><?php the_title(); ?></p>
+                                <div class="year-location">
+                                    <span><?php echo get_the_date('Y'); ?> -
+                                        <?php the_field('location'); ?></span>
+                                </div>
+                            </a>
+                        </div>
+
+                    <?php } ?>
                 </div>
-                <a href="" class="project-info">
-                    <p class="project-name">Project Name</p>
-                    <div class="year-location">
-                        <span>2022 - Dubai</span>
-                    </div>
-                </a>
-            </div>
-            <div class="relatedProjectsWrap">
-                <div class="projectMedia">
-                    <img src="./assets/img/sample/project-03.webp" alt="">
-                </div>
-                <a href="" class="project-info">
-                    <p class="project-name">Project Name</p>
-                    <div class="year-location">
-                        <span>2022 - Dubai</span>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </section>
+            </section>
+        <?php }
+        wp_reset_postdata();
+    }
+    ?>
+
 </main>
 <?php get_footer();
