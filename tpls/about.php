3bbb4464c0de537f350c544dc526ba1ddf423052
alireza <alirezaallahyarian@gmail.com>
2024-09-28 16:44:51 +0330

    Preparing the About Us page to be dynamic

--- tpls/about.php
+++ tpls/about.php
@@ -1,3 +1,4 @@
+<?php get_header(); ?>
 <main class="wrapper aboutWrapper">
     <div id="newsletterLink-container">
         <span>Monthly Newsletter</span>
@@ -8,9 +9,9 @@
     </div>
     <section id="aboutWrapper">
         <section class="logo-anchor-links">
-<!--            <div class="logo-items">-->
-<!--                <h1>Designex</h1>-->
-<!--            </div>-->
+            <!--            <div class="logo-items">-->
+            <!--                <h1>Designex</h1>-->
+            <!--            </div>-->
             <div class="aboutAnchorLinksWrap">
                 <img src="./assets/img/elm.svg" alt="">
                 <div id="aboutAnchorLinks">
@@ -37,39 +38,56 @@
             <div class="aboutSloganInfo">
                 <h2>Crafting Luxury, Driven by Research, Delivering Excellence from Concept to Completion.</h2>
                 <div class="content">
-                    <p>Communicate that Designex offers a full spectrum of services, from luxury renovation to urban planning and environmental studies, all grounded in research and executed with excellence.</p>
-                    <p>Communicate that Designex offers a full spectrum of services, from luxury renovation to urban planning and environmental studies, all grounded in research and executed with excellence.</p>
+                    <p>Communicate that Designex offers a full spectrum of services, from luxury renovation to urban
+                        planning and environmental studies, all grounded in research and executed with excellence.</p>
+                    <p>Communicate that Designex offers a full spectrum of services, from luxury renovation to urban
+                        planning and environmental studies, all grounded in research and executed with excellence.</p>
                 </div>
             </div>
         </section>
         <section class="whyUsContainer paddingAboutX" id="whyUs">
             <div class="whyUsTitle aboutTitle">
-                <h2>Choosing Designex for your design and construction needs comes with a multitude of distinct advantages:</h2>
+                <h2>Choosing Designex for your design and construction needs comes with a multitude of distinct
+                    advantages:</h2>
             </div>
             <div class="whyUsDescription">
                 <div class="descriptionSteps">
                     <h3>World-class Expertise:</h3>
-                    <p>Our team comprises professionals with extensive experience in delivering luxury projects in Dubai. Additionally, we collaborate with leading international universities and experts to ensure the highest quality and reliability across all our services, including new ventures into urban design and environmental sustainability.</p>
+                    <p>Our team comprises professionals with extensive experience in delivering luxury projects in
+                        Dubai. Additionally, we collaborate with leading international universities and experts to
+                        ensure the highest quality and reliability across all our services, including new ventures into
+                        urban design and environmental sustainability.</p>
                 </div>
                 <div class="descriptionSteps">
                     <h3>Comprehensive Services:</h3>
-                    <p>We offer a wide array of services from architectural design, interior design, and construction management to urban planning, master planning, environmental impact assessments, and sustainability appraisals. Our diverse services make us your go-to partner for all your architectural and design needs, from luxury villas to sustainable urban developments.</p>
+                    <p>We offer a wide array of services from architectural design, interior design, and construction
+                        management to urban planning, master planning, environmental impact assessments, and
+                        sustainability appraisals. Our diverse services make us your go-to partner for all your
+                        architectural and design needs, from luxury villas to sustainable urban developments.</p>
                 </div>
                 <div class="descriptionSteps">
                     <h3>Eco-friendly Luxury:</h3>
-                    <p>Our unique vision of luxury incorporates respect for both humanity and the environment. Our design ethos centres on creating luxurious spaces in harmony with nature, promoting sustainable living, and driving ecological responsibility.</p>
+                    <p>Our unique vision of luxury incorporates respect for both humanity and the environment. Our
+                        design ethos centres on creating luxurious spaces in harmony with nature, promoting sustainable
+                        living, and driving ecological responsibility.</p>
                 </div>
                 <div class="descriptionSteps">
                     <h3>Cost-effective Solutions:</h3>
-                    <p>Despite our emphasis on high-end sustainable design, we offer cost-effective solutions, ensuring that quality and luxury are accessible within your budget. Our extensive experience in design and construction enables us to deliver sustainable luxury that balances beauty with functionality.</p>
+                    <p>Despite our emphasis on high-end sustainable design, we offer cost-effective solutions, ensuring
+                        that quality and luxury are accessible within your budget. Our extensive experience in design
+                        and construction enables us to deliver sustainable luxury that balances beauty with
+                        functionality.</p>
                 </div>
                 <div class="descriptionSteps">
                     <h3>Cultural Sensitivity:</h3>
-                    <p>With a deep understanding of local and international cultures, Designex delivers projects that resonate with both local aesthetics and global best practices.</p>
+                    <p>With a deep understanding of local and international cultures, Designex delivers projects that
+                        resonate with both local aesthetics and global best practices.</p>
                 </div>
                 <div class="descriptionSteps">
                     <h3>Customized Approach:</h3>
-                    <p>We tailor every project to its unique context and the client's vision, enhancing the quality of life while striking a balance with the environment. Our projects aim to set a new standard for quality living and urban development in Dubai and beyond.</p>
+                    <p>We tailor every project to its unique context and the client's vision, enhancing the quality of
+                        life while striking a balance with the environment. Our projects aim to set a new standard for
+                        quality living and urban development in Dubai and beyond.</p>
                 </div>
             </div>
         </section>
@@ -81,11 +99,19 @@
                 <div class="missionVisionInfoWrapper">
                     <div class="missionVisionContent">
                         <h3>Our<br> Mission</h3>
-                        <p>Our mission is to provide comprehensive design and construction services that blend functionality, aesthetics, research, and sustainability. We are committed to delivering projects on time, within budget, and to the highest quality standards. Designex seeks to foster long-term partnerships with clients based on trust, transparency, and shared success.</p>
+                        <p>Our mission is to provide comprehensive design and construction services that blend
+                            functionality, aesthetics, research, and sustainability. We are committed to delivering
+                            projects on time, within budget, and to the highest quality standards. Designex seeks to
+                            foster long-term partnerships with clients based on trust, transparency, and shared success.
+                        </p>
                     </div>
                     <div class="missionVisionContent">
                         <h3>Our<br> vision</h3>
-                        <p>Designex envisions being a trusted partner in creating spaces that inspire and positively impact people’s lives. Our goal is to be recognised as a leading design and construction firm, not just in Dubai, but internationally, as we expand into urban planning, master planning, and environmental studies. We aim to be at the forefront of designing vibrant, sustainable communities that reflect the highest standards of quality.</p>
+                        <p>Designex envisions being a trusted partner in creating spaces that inspire and positively
+                            impact people’s lives. Our goal is to be recognised as a leading design and construction
+                            firm, not just in Dubai, but internationally, as we expand into urban planning, master
+                            planning, and environmental studies. We aim to be at the forefront of designing vibrant,
+                            sustainable communities that reflect the highest standards of quality.</p>
                     </div>
                 </div>
                 <div class="missionVisionSliderWrapper">
@@ -115,12 +141,28 @@
             </div>
             <div class="statementWrapper">
                 <div class="statementInfoWrapper">
-                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation and sustainability. Rooted in the rich tradition of architectural design and construction, Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury and quality without compromise.</p>
-                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas, apartments, and commercial spaces. Now, we are expanding our expertise into broader realms, including urban planning, master planning, and environmental projects that enhance not just individual buildings but entire communities.</p>
-                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into sustainable and environmentally respectful projects that meet the highest standards of luxury. Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
-                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation and sustainability. Rooted in the rich tradition of architectural design and construction, Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury and quality without compromise.</p>
-                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas, apartments, and commercial spaces. Now, we are expanding our expertise into broader realms, including urban planning, master planning, and environmental projects that enhance not just individual buildings but entire communities.</p>
-                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into sustainable and environmentally respectful projects that meet the highest standards of luxury. Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
+                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation
+                        and sustainability. Rooted in the rich tradition of architectural design and construction,
+                        Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury
+                        and quality without compromise.</p>
+                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas,
+                        apartments, and commercial spaces. Now, we are expanding our expertise into broader realms,
+                        including urban planning, master planning, and environmental projects that enhance not just
+                        individual buildings but entire communities.</p>
+                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into
+                        sustainable and environmentally respectful projects that meet the highest standards of luxury.
+                        Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
+                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation
+                        and sustainability. Rooted in the rich tradition of architectural design and construction,
+                        Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury
+                        and quality without compromise.</p>
+                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas,
+                        apartments, and commercial spaces. Now, we are expanding our expertise into broader realms,
+                        including urban planning, master planning, and environmental projects that enhance not just
+                        individual buildings but entire communities.</p>
+                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into
+                        sustainable and environmentally respectful projects that meet the highest standards of luxury.
+                        Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
                 </div>
                 <div class="statementSliderWrapper" data-lenis-prevent>
                     <div class="swiper mySwiper">
@@ -146,13 +188,22 @@
                     <span>CEO, Founder</span>
                 </div>
                 <div class="founderContent">
-                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation and sustainability. Rooted in the rich tradition of architectural design and construction, Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury and quality without compromise.</p>
-                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas, apartments, and commercial spaces. Now, we are expanding our expertise into broader realms, including urban planning, master planning, and environmental projects that enhance not just individual buildings but entire communities.</p>
-                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into sustainable and environmentally respectful projects that meet the highest standards of luxury. Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
+                    <p>Choose Designex because we epitomise the highest quality craftsmanship with a focus on innovation
+                        and sustainability. Rooted in the rich tradition of architectural design and construction,
+                        Designex combines timeless aesthetics with cutting-edge sustainable methods, delivering luxury
+                        and quality without compromise.</p>
+                    <p>Designex has earned a reputation in Dubai for its luxurious and meticulously designed villas,
+                        apartments, and commercial spaces. Now, we are expanding our expertise into broader realms,
+                        including urban planning, master planning, and environmental projects that enhance not just
+                        individual buildings but entire communities.</p>
+                    <p>With Designex, experience the elegance of superior design incorporated seamlessly into
+                        sustainable and environmentally respectful projects that meet the highest standards of luxury.
+                        Choose Designex – where quality meets luxury, and innovation drives sustainability.</p>
                 </div>
             </div>
             <div class="founderVideoWrapper">
-                <video id="founderVideo" autoplay muted loop playsinline preload="auto" poster="./assets/img/sample/founder-poster.jpg" data-url="./assets/video/founder.mp4">
+                <video id="founderVideo" autoplay muted loop playsinline preload="auto"
+                    poster="./assets/img/sample/founder-poster.jpg" data-url="./assets/video/founder.mp4">
                     <source src="./assets/video/founder.mp4" type="video/mp4">
                 </video>
                 <div id="playFounder">
@@ -225,7 +276,8 @@
             </section>
             <section class="aboutOfficeContainer">
                 <picture>
-                    <source srcset="./assets/img/sample/about-full-image.jpg" media="(max-width: 600px)"> <!-- medium size -->
+                    <source srcset="./assets/img/sample/about-full-image.jpg" media="(max-width: 600px)">
+                    <!-- medium size -->
                     <img src="./assets/img/sample/about-full-image.jpg" alt=""> <!-- Original Size -->
                 </picture>
             </section>
@@ -340,7 +392,11 @@
                 </div>
                 <div class="theFeatureContent">
                     <h3>Mr. <br>Someone</h3>
-                    <p>Designex envisions being a trusted partner in creating spaces that inspire and positively impact people’s lives. Our goal is to be recognised as a leading design and construction firm, not just in Dubai, but internationally, as we expand into urban planning, master planning, and environmental studies. We aim to be at the forefront of designing vibrant, sustainable communities that reflect the highest standards of quality.</p>
+                    <p>Designex envisions being a trusted partner in creating spaces that inspire and positively impact
+                        people’s lives. Our goal is to be recognised as a leading design and construction firm, not just
+                        in Dubai, but internationally, as we expand into urban planning, master planning, and
+                        environmental studies. We aim to be at the forefront of designing vibrant, sustainable
+                        communities that reflect the highest standards of quality.</p>
                 </div>
             </div>
             <div class="scrolling-wrap">
@@ -641,3 +697,4 @@
         <span>close</span>
     </div>
 </div>
+<?php get_footer();
