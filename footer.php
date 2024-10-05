28736f2d2ebd9c5a3b5e018d26a292db3e098e49
Fatemeh Basti <ftmbasti@gmail.com>
2024-10-05 18:06:55 +0330

    change add loading script

--- footer.php
+++ footer.php
@@ -112,7 +112,7 @@
 <?php if(is_front_page()):?>
 <script defer src="<?php ThemeAssets('js/loading.js') ?>"></script>
 <?php else:?>
-    <script>  $(document).ready(function() {  setTimeout(function() {  $('html').addClass('loadingDone');  }, 2000); // 2000 milliseconds = 2 seconds  });  </script>
+    <script>  $(document).ready(function() {  setTimeout(function() {  $('html').addClass('loadingDone');  }, 2000);   });  </script>
 <?php endif;
 if (is_singular('projects') or is_page_template('tpls/about.php')): ?>
     <script defer src="<?php ThemeAssets('js/swiper-bundle.min.js'); ?>"></script>
