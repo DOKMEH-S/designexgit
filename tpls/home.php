9e4dfdc4c1158dea306b26b3cf9ea526f7e4e9f4
graymicro <alirezaallahyarian@gmail.com>
2024-10-07 16:56:43 +0330

    dynamic home

--- tpls/home.php
+++ tpls/home.php
@@ -3,246 +3,95 @@ get_header(); ?>
 
 
 <section class="homeSideBarContainer">
-  <h1 class="home-title">Redefining <strong>luxury architecture</strong> through <strong>sustainable</strong>, opulent environmental <strong>responsibility</strong>.</h1>
-  <div class="homeElementCounterWrapper">
-    <div id="counterBox" class="counter">
-      <span id="counterValue">01</span>
-      <span id="totalProject"></span>
+    <h1 class="home-title">Redefining <strong>luxury architecture</strong> through <strong>sustainable</strong>, opulent
+        environmental <strong>responsibility</strong>.</h1>
+    <div class="homeElementCounterWrapper">
+        <div id="counterBox" class="counter">
+            <span id="counterValue">01</span>
+            <span id="totalProject"></span>
+        </div>
+        <a href="" id="currentProjectName">
+            <p></p>
+        </a>
+        <div class="loading-container getScale getRotate getNumber loseNumber">
+            <div class="loading_line">
+                <div class="loading-point_canvas one"></div>
+                <div class="loading-point"></div>
+                <div class="loading-point_canvas two"></div>
+            </div>
+        </div>
     </div>
-    <a href="" id="currentProjectName"><p></p></a>
-    <div class="loading-container getScale getRotate getNumber loseNumber">
-      <div class="loading_line">
-        <div class="loading-point_canvas one"></div>
-        <div class="loading-point"></div>
-        <div class="loading-point_canvas two"></div>
-      </div>
+    <div class="homeVideoItemsWrapper">
+        <div class="homeVideoContainer">
+            <div id="mediaHomeWrap">
+                <img id="mediaHomeProject" src="" alt="">
+            </div>
+            <h2>Redefining <strong>luxury architecture</strong> through <strong>sustainable</strong>, opulent
+                environmental <strong>responsibility</strong>.</h2>
+        </div>
+        <div class="homeItemsWrapper">
+            <div class="homeItemsWrap">
+                <h3>Monthly Newsletter</h3>
+                <a href="" class="">Subscribe here</a>
+            </div>
+            <div class="homeItemsWrap">
+                <h3>do you have a project?</h3>
+                <a href="" class="">whatsapp</a>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="homeVideoItemsWrapper">
-    <div class="homeVideoContainer">
-      <div id="mediaHomeWrap">
-        <img id="mediaHomeProject" src="" alt="">
-      </div>
-      <h2>Redefining <strong>luxury architecture</strong> through <strong>sustainable</strong>, opulent environmental <strong>responsibility</strong>.</h2>
-    </div>
-    <div class="homeItemsWrapper">
-      <div class="homeItemsWrap">
-        <h3>Monthly Newsletter</h3>
-        <a href="" class="">Subscribe here</a>
-      </div>
-      <div class="homeItemsWrap">
-        <h3>do you have a project?</h3>
-        <a href="" class="">whatsapp</a>
-      </div>
-    </div>
-  </div>
 </section>
 <div id="smooth-wrapper">
-  <div id="smooth-content">
-    <main class="wrapper homeWrapper">
-      <div id="newsletterLink-container">
-        <span>Monthly Newsletter</span>
-        <a href="">
-          <img src="./assets/img/link.svg" alt="link">
-          Subscribe here
-        </a>
-      </div>
-      <section class="homeProjectContainer">
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name01</h2>
-              <span class="project_year">2012/</span>
+    <div id="smooth-content">
+        <main class="wrapper homeWrapper">
+            <div id="newsletterLink-container">
+                <span>Monthly Newsletter</span>
+                <a href="">
+                    <img src="./assets/img/link.svg" alt="link">
+                    Subscribe here
+                </a>
             </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-5.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-2.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name02</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-6.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-3.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name03</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name04</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-5.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-2.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name05</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-6.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-3.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name06</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name07</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-5.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-2.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name08</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-6.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-3.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name09</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name10</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-5.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-2.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name11</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-6.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-3.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name12</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name13</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-5.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-2.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name14</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-6.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-3.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name15</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-        <a href="single-project.html" class="homeProjectWrap" data-url="./assets/img/sample/project-4.png">
-          <div class="projectMedia">
-            <img src="./assets/img/sample/home-project-1.jpg" alt="">
-          </div>
-          <div class="projectInfo">
-            <img src="./assets/img/sample/NAH_ICON%204.png" alt="" class="logo">
-            <div class="title-year">
-              <h2 class="project_name">/project name16</h2>
-              <span class="project_year">2012/</span>
-            </div>
-          </div>
-        </a>
-      </section>
-    </main>
-  </div>
+            <?php
+            $projects = get_field('project'); 
+            if ($projects): ?>
+                <section class="homeProjectContainer">
+                    <?php
+                    foreach ($projects as $post):
+                        setup_postdata($post);
+                        $project_logo = get_field('project_logo');
+                        $sketch_image = get_field('sketch_image');
+                        $categories = get_the_terms($post->ID, 'category');
+                        $project_year = '';
+                        if ($categories) {
+                            foreach ($categories as $category) {
+                                if ($category->term_id == 5) {
+                                    $project_year = $category->name;
+                                    break;
+                                }
+                            }
+                        }
+                        ?>
+                        <a href="<?php the_permalink(); ?>" class="homeProjectWrap"
+                            data-url="<?php echo esc_url($sketch_image['sizes']['large']); ?>">
+                            <div class="projectMedia">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="projectInfo">
+                                <img src="<?php echo esc_url($project_logo['sizes']['thumbnail']); ?>" alt="<?php echo $project_logo['alt'];?>" class="logo">
+                                <div class="title-year">
+                                    <h2 class="project_name">/<?php the_title(); ?></h2>
+                                    <span class="project_year"><?php echo esc_html($project_year); ?>/</span>
+                                </div>
+                            </div>
+                        </a>
+                        <?php
+                    endforeach;
+                    wp_reset_postdata();
+                    ?>
+                </section>
+            <?php endif; ?>
+        </main>
+    </div>
 </div>
 
 <?php get_footer();
