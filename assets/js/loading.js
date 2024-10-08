e010a30efdbf4b06eb258bc8abb510ad193e96a0
s.alamdari <s.alamdari.98@gmail.com>
2024-10-08 10:20:30 +0330

    update loading assets

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,42 +1,42 @@
-const loadingContainer = document.querySelector('.loading-container');
+// const loadingContainer = document.querySelector('.loading-container');
+//
+// // setTimeout(() => {
+// //     loadingContainer.classList.add('getScale');
+// // }, 0);
+//
+// setTimeout(() => {
+//     loadingContainer.classList.add('getRotate');
+// }, 100);
+//
+// setTimeout(() => {
+//     loadingContainer.classList.add('getNumber');
+//
+//     // Add loseNumber class after 1 second
+//     setTimeout(() => {
+//         loadingContainer.classList.add('loseNumber');
+//     }, 1000); // 1000 milliseconds = 1 second
+//
+// }, 100);
+//
+// setTimeout(() => {
+//     const counter = document.querySelectorAll('.percentCounter');
+//     counter.forEach(item => {
+//         let counterInnerText = parseInt(item.textContent, 10);
+//         let count = 1;
+//         let speed = item.dataset.speed / counterInnerText;
+//
+//         function counterUp() {
+//             item.textContent = count++ + '%'; // Add % here
+//             if (count > counterInnerText) {
+//                 clearInterval(stop);
+//             }
+//         }
+//
+//         const stop = setInterval(counterUp, speed);
+//     });
+// }, 500);
 
 // setTimeout(() => {
-//     loadingContainer.classList.add('getScale');
-// }, 0);
-
-setTimeout(() => {
-    loadingContainer.classList.add('getRotate');
-}, 100);
-
-setTimeout(() => {
-    loadingContainer.classList.add('getNumber');
-
-    // Add loseNumber class after 1 second
-    setTimeout(() => {
-        loadingContainer.classList.add('loseNumber');
-    }, 1000); // 1000 milliseconds = 1 second
-
-}, 100);
-
-setTimeout(() => {
-    const counter = document.querySelectorAll('.percentCounter');
-    counter.forEach(item => {
-        let counterInnerText = parseInt(item.textContent, 10);
-        let count = 1;
-        let speed = item.dataset.speed / counterInnerText;
-
-        function counterUp() {
-            item.textContent = count++ + '%'; // Add % here
-            if (count > counterInnerText) {
-                clearInterval(stop);
-            }
-        }
-
-        const stop = setInterval(counterUp, speed);
-    });
-}, 500);
-
-setTimeout(() => {
-    // Add any final class to indicate loading is done
-    document.querySelector('html').classList.add('loadingDone');
-}, 1500);
+//     // Add any final class to indicate loading is done
+//     document.querySelector('html').classList.add('loadingDone');
+// }, 500);
--- header.php
+++ header.php
@@ -25,18 +25,6 @@
                 display: none;
             }
         <?php endif; ?>
-        <?php if (is_front_page()): ?>
-            #lottie {
-                background-color: #ffffff;
-                width: 100%;
-                height: 100%;
-                display: block;
-                overflow: hidden;
-                transform: translate3d(0, 0, 0);
-                text-align: center;
-                opacity: 1;
-            }
-        <?php endif; ?>
     </style>
     <link href="<?php ThemeAssets('css/fonts.css'); ?>" rel="stylesheet" as="style"
         onload="this.onload=null;this.rel='stylesheet'">
