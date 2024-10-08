87f48d3e5d21e3a75f4ea1a4cd7836458bbd1407
Fatemeh Basti <f.basti@dokmeh.local>
2024-10-08 15:06:07 +0330

    remove css newsletter

--- functions.php
+++ functions.php
@@ -99,8 +99,8 @@ function Dokmeh_scripts()
         );
     endif;
 
-    wp_dequeue_style('newsletter');
-    wp_deregister_style('newsletter');
+//    wp_dequeue_style('newsletter');
+//    wp_deregister_style('newsletter');
 // // --- remove contact form 7 files from pages doesn't have form ----------
 //     if( is_front_page() OR is_page_template('tpls/contact.php') ) {
 //         wp_enqueue_script('contact-form-7');
@@ -337,8 +337,8 @@ function blog_filter_handler()
 add_action('wp_ajax_blog_filter', 'blog_filter_handler');
 add_action('wp_ajax_nopriv_blog_filter', 'blog_filter_handler');
 
-//add_action( 'wp_enqueue_scripts', function() {
-//    wp_dequeue_style('newsletter-css');
-//    wp_deregister_style('newsletter-css');
-//}, 1);
+add_action( 'wp_enqueue_scripts', function() {
+    wp_dequeue_style('newsletter-css');
+    wp_deregister_style('newsletter-css');
+}, 100);
 
