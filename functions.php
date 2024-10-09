90411b1298982ff1c5a6d0527e7f7f95f3ab5f9f
dokmeh <nikooaqdas@gmail.com>
2024-10-09 22:30:21 +0330

    increase data version function

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function Dokmeh_theme_setup()
 {
     if (!defined('_S_VERSION')) {
-        define('_S_VERSION', '1.0.4');
+        define('_S_VERSION', '1.0.5');
     }
     $menus = array(
         'main-menu' => 'Main Menu',
