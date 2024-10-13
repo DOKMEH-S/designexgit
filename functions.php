864b922553395caa58f26831872ff22dd5fa6075
s.alamdari <s.alamdari.98@gmail.com>
2024-10-13 14:50:48 +0330

    dragability is done 3

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function Dokmeh_theme_setup()
 {
     if (!defined('_S_VERSION')) {
-        define('_S_VERSION', '1.0.9');
+        define('_S_VERSION', '1.1.0');
     }
     $menus = array(
         'main-menu' => 'Main Menu',
